10571160b656ebbbfd0bbafa79d3e2dd3a8db5be
danchoihecode <96305614+danchoihecode@users.noreply.github.com>
2023-07-06 20:12:21 +0700

    Add files via upload

---
+++ LichSuVietNam/src/demo/Test.java
@@ -0,0 +1,11 @@
+package demo;
+
+import java.io.File;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(new File("file\\figure-source-2.json").getAbsolutePath());
+	}
+
+}
