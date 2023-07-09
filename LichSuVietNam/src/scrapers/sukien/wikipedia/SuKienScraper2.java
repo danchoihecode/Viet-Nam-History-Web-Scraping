4ce2a88ce94caa93aea584bdd7e847c4cfd714af
T2Dzung <93850123+T2Dzung@users.noreply.github.com>
2023-07-10 01:50:16 +0700

    Add files via upload

--- LichSuVietNam/src/scrapers/sukien/wikipedia/SuKienScraper2.java
+++ LichSuVietNam/src/scrapers/sukien/wikipedia/SuKienScraper2.java
@@ -3,34 +3,35 @@ package scrapers.sukien.wikipedia;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class SuKienScraper2 extends Scraper implements GetData {
+public class SuKienScraper2{
 	
-	@Override
-	public void getData() {
-		getSavedUrl();
-		
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("file\\event-source-2.txt"));
-			String line = reader.readLine();
-			while (line != null) {
-				try {
-					Document doc = Jsoup.connect(line).get();
-					System.out.println(line);
-					if(!suKienJson.contains(line)) scrape(line, doc);	
-				} catch (IOException e) {
-					System.out.println("Không thể kết nối tới trang web "+line);
-				}		
-				
-				line = reader.readLine();
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//	@Override
+//	public void getData() {
+//		getSavedUrl();
+//		
+//		try {
+//			BufferedReader reader = new BufferedReader(new FileReader("file\\event-source-2.txt"));
+//			String line = reader.readLine();
+//			while (line != null) {
+//				try {
+//					Document doc = Jsoup.connect(line).get();
+//					System.out.println(line);
+//					if(!suKienJson.contains(line)) scrape(line, doc);	
+//				} catch (IOException e) {
+//					System.out.println("Không thể kết nối tới trang web "+line);
+//				}		
+//				
+//				line = reader.readLine();
+//			}
+//			reader.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		
 //		String line = "https://vi.wikipedia.org/wiki/Tr%E1%BA%ADn_%C4%90%E1%BB%93_B%C3%A0n_(1377)";
 //		try {
@@ -40,15 +41,25 @@ public class SuKienScraper2 extends Scraper implements GetData {
 //			System.out.println("Không thể kết nối tới trang web." + line);
 //			return;
 //		}	
-		save("file\\event-source-2-2.json");
-	}
+//		save("file\\event-source-2-2.json");
+//	}
 	
 	
 	
 	public static void main(String[] args) {
-		SuKienScraper2 scraper = new SuKienScraper2();
-		
-		scraper.getData();
+		SuKienWiki scraper = new SuKienWiki() {
+			@Override
+			public void scrape(Document doc) {
+				scrapeWiki(doc);
+				System.out.println(doc.baseUri());
+			}
+		};
+		ArrayList<Document> docs = scraper.connectToUrls("file\\event-source-2.txt");
+		for (Document doc : docs) {
+				scraper.scrape(doc);
+		}
+		scraper.getJsonString();
+		scraper.saveToFile("file\\event-source-2.json", true);
 	}
 
 }
